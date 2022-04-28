3fbb019e259ed8be049817ef1c6174d5cce0e990
Chiro Liang <Chiro2001@163.com>
2022-04-28 14:53:30 +0800

    Booth test done

--- src/main/scala/booth/Booth.scala
+++ src/main/scala/booth/Booth.scala
@@ -13,9 +13,10 @@ class Booth(widthInput: Int = 16) extends Module {
     val start = Input(Bool())
     val z = Output(UInt((widthInput * 2).W))
   })
-  val a = RegInit(0.U((widthInput * 2).W))
-  val qLast = RegInit(false.B)
-  val cnt = RegInit(0.U(5.W))
+  val a = RegInit(0.U(widthInput.W))
+  val q = RegInit(0.U(widthInput.W))
+  val qExtra = RegInit(false.B)
+  val cnt = RegInit(0.U(8.W))
 
   val states = Enum(3)
   val idleState :: runningState :: outputState :: Nil = states
@@ -23,7 +24,7 @@ class Booth(widthInput: Int = 16) extends Module {
   val nextState = Wire(UInt(log2Ceil(states.size).W))
   val stateMatrix = Array(
     idleState -> Mux(io.start, runningState, idleState),
-    runningState -> Mux(cnt === 16.U, outputState, runningState),
+    runningState -> Mux(cnt === (widthInput - 1).U, outputState, runningState),
     outputState -> idleState
   )
   nextState := MuxLookup(state, idleState, stateMatrix)
@@ -34,25 +35,37 @@ class Booth(widthInput: Int = 16) extends Module {
   val x = io.x
   val minusX = -io.x
 
-  def aLast = a(widthInput * 2 - 1).asBool
+  def qLast = q(0).asBool
 
-  def pack = VecInit(Seq(aLast, qLast)).asUInt
+  def pack = VecInit(Seq(qLast, qExtra).reverse).asUInt
 
   def addShift(addValue: UInt) = {
-    qLast := aLast
-    a := (((a + VecInit(Seq(addValue, 0.U(widthInput.W))).asUInt).asTypeOf(SInt(a.getWidth.W))) >> 1.U).asUInt
+    // val aNext = (((a + VecInit(Seq(addValue, 0.U(widthInput.W))).asUInt).asTypeOf(SInt(a.getWidth.W))) >> 1.U).asUInt
+    val aNext = (a + addValue).asUInt
+    val aqNext = (VecInit(Seq(aNext, q).reverse).asUInt.asSInt >> 1.U).asUInt
+    val qExtraNext = qLast
+    qExtra := qExtraNext
+    a := aqNext(widthInput * 2 - 1, widthInput)
+    q := aqNext(widthInput - 1, 0)
+    printf("a: %b -> %b; aqNext: %b; q_-1: %b -> %b; addValue = %b\n", a, aNext, aqNext, qExtra, qExtraNext, addValue)
   }
 
-  when (state === runningState) {
-    io.busy := true.B
-    addShift(MuxLookup(pack, 0.U, Array(
-      "b01".U -> x,
-      "b10".U -> minusX
-    )))
-    cnt := cnt + 1.U
-  } .otherwise {
-    when (state === outputState) {
-      io.z := a
+  switch(state) {
+    is(idleState) {
+      q := io.y
+      cnt := 0.U
+    }
+    is(runningState) {
+      io.busy := true.B
+      printf("[%d state=%d] a=%b, q=%b, qExtra=%b, pack=%b, x=%b, -x=%b\n", cnt, state, a, q, qExtra, pack, x, minusX)
+      addShift(MuxLookup(pack, 0.U, Array("b01".U -> x, "b10".U -> minusX)))
+      cnt := cnt + 1.U
+    }
+    is(outputState) {
+      cnt := 0.U
+      val result = VecInit(Seq(a, q).reverse).asUInt
+      io.z := result
+      printf("result = %b\n", result)
     }
   }
 }
--- src/test/scala/booth/BoothTest.scala
+++ src/test/scala/booth/BoothTest.scala
@@ -9,17 +9,19 @@ import org.scalatest.matchers.should
 class BoothTest extends AnyFlatSpec with ChiselScalatestTester with should.Matchers {
   behavior of "Booth"
   it should "pass multiply test" in {
-    test(new Booth) { d =>
+    val widthInput = 4
+    test(new Booth(widthInput)) { d =>
       println("generate RTL done")
-      d.io.x.poke(10.U)
-      d.io.y.poke(2.U)
+      d.io.x.poke("b0110".U)
+      d.io.y.poke("b0101".U)
       d.clock.step(5)
       d.io.start.poke(true.B)
       d.clock.step(1)
       d.io.busy.expect(true.B)
-      d.clock.step(16)
+      d.io.start.poke(false.B)
+      d.clock.step(widthInput)
       d.io.busy.expect(false.B)
-      d.io.z.expect(20.U)
+      d.io.z.expect(30.U)
     }
   }
 }
